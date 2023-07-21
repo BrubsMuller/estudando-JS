508b99c74f313c6aca6c3f62b7e74e2ffc2ed527
BrubsMuller <muller.vendeta@hotmail.com>
2023-07-21 14:18:25 -0300

    atividade

---
+++ aula-210723/atividade/livros.js
@@ -0,0 +1,64 @@
+/*Crie um arquivo JavaScript chamado "livros.js" que será responsável por gerenciar os dados dos livros. Neste arquivo, você deve exportar uma vetor de livros que amazene objetos com as propriedades: título, autor, ano de publicação e gêner*/
+
+export const livros = [
+    {
+        titulo: "Dom Quixote",
+        autor: "Miguel de Cervantes",
+        anoPublicacao: 1605,
+        genero: "Romance de Cavalaria"
+    },
+    {
+        titulo: "Orgulho e Preconceito",
+        autor: "Jane Austen",
+        anoPublicacao: 1813,
+        genero: "Romance"
+    },
+    {
+        titulo: "1984",
+        autor: "George Orwell",
+        anoPublicacao: 1949,
+        genero: "Ficção Distópica"
+    },
+    {
+        titulo: "O Senhor dos Anéis",
+        autor: "J.R.R. Tolkien",
+        anoPublicacao: 1954,
+        genero: "Fantasia Épica"
+    },
+    {
+        titulo: "O Pequeno Príncipe",
+        autor: "Antoine de Saint-Exupéry",
+        anoPublicacao: 1943,
+        genero: "Literatura Infantil"
+    },
+    {
+        titulo: "Cem Anos de Solidão",
+        autor: "Gabriel García Márquez",
+        anoPublicacao: 1967,
+        genero: "Realismo Mágico"
+    },
+    {
+        titulo: "O Sol é para Todos",
+        autor: "Harper Lee",
+        anoPublicacao: 1960,
+        genero: "Ficção Clássica"
+    },
+    {
+        titulo: "A Revolução dos Bichos",
+        autor: "George Orwell",
+        anoPublicacao: 1945,
+        genero: "Sátira Política"
+    },
+    {
+        titulo: "Harry Potter e a Pedra Filosofal",
+        autor: "J.K. Rowling",
+        anoPublicacao: 1997,
+        genero: "Fantasia Juvenil"
+    },
+    {
+        titulo: "A Menina que Roubava Livros",
+        autor: "Markus Zusak",
+        anoPublicacao: 2005,
+        genero: "Ficção Histórica"
+    }
+];
